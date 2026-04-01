6e23d11df817639034d6377437e0456888122287
Yeasin-Riyad <mazumderyeasin98@gmail.com>
2026-04-01 21:48:47 +0600

    Authentication & Authorization Part Complete

--- server/server.js
+++ server/server.js
@@ -8,3 +8,14 @@ connectToDB()
 app.listen(3000, () => {
     console.log("Server is running on port 3000")
 })
+
+
+// We can also create a sever like this:-
+// const http = require("http")
+// const app = require("./src/app")
+
+// const server = http.createServer(app)
+
+// server.listen(3000, () => {
+//     console.log("Server is running on port 3000")
+// })
